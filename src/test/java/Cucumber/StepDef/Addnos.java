9a6d824f9319e4852e5a9040c213ef68909af5a9
sukanya <sukanya@m.com>
2020-05-18 18:45:52 +0530

    commited

---
+++ src/test/java/Cucumber/StepDef/Addnos.java
@@ -0,0 +1,10 @@
+package Cucumber.StepDef;
+
+public class Addnos {
+    public void Add()
+    {
+        int a=10,b=5,c;
+        c=a+b;
+        System.out.println("adding of 2 numbers"+c);
+    }
+}
