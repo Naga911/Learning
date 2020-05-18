b7b50fd2961d5d305f0a9b32d5c6f1d7ddb2da78
RanjithBillakanti <ranjitbillakanti@gmail.com>
2020-05-18 18:46:53 +0530

    commiting

---
+++ src/test/java/Cucumber/Percentage.java
@@ -0,0 +1,10 @@
+package Cucumber;
+
+public class Percentage {
+    public void perc()
+    {
+        int a=10,b=5,c;
+        c=a%b;
+        System.out.println("Percentage of 2 numbers"+c);
+    }
+}
